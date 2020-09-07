e5c331f1a6f52c3865cddab6bb221b39ae9ef3ec
BOBO1997git <yh198595@gmail.com>
2020-09-08 03:29:15 +0900

    fixed bug : ASSIGN -> BANG

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -120,7 +120,7 @@ func (l *Lexer) NextToken() token.Token {
 			l.readChar()
 			tok = token.Token{Type: token.NEQ, Literal: string(ch) + string(l.ch)}
 		} else {
-			tok = newToken(token.ASSIGN, l.ch)
+			tok = newToken(token.BANG, l.ch)
 		}
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
