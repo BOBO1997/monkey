551b71c7cabcb17ccadc5158d6a926148c86b678
BOBO1997git <yh198595@gmail.com>
2020-09-09 04:50:09 +0900

    added boolean evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/BOBO1997/monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
 // Eval function
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -14,15 +20,26 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
 }
 
+// evalStatements function
 func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 	for _, stmt := range stmts {
 		result = Eval(stmt)
 	}
 	return result
+}
+
+// nativeBoolToBooleanObject function
+func nativeBoolToBooleanObject(value bool) object.Object {
+	if value {
+		return TRUE
+	}
+	return FALSE
 }
